ded83d199014aad58c1e89d7f7494703c9216c07
wwwwwwwww259 <wakshakob@gmail.com>
2026-06-11 17:57:48 +0800

    auth.js

--- js/auth.js
+++ js/auth.js
@@ -2,35 +2,66 @@ import { db } from "./firebase.js";
 
 import {
   collection,
-  addDoc
+  addDoc,
+  getDocs
 } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
 const saveBtn = document.getElementById("saveBtn");
 
-if (saveBtn) {
+async function loadEmployees(){
 
-  saveBtn.addEventListener("click", async () => {
+  const employeeList =
+  document.getElementById("employeeList");
 
-    const name = document.getElementById("name").value;
-    const position = document.getElementById("position").value;
-    const salary = document.getElementById("salary").value;
+  employeeList.innerHTML = "";
 
-    try {
+  const querySnapshot =
+  await getDocs(collection(db,"employees"));
 
-      await addDoc(collection(db, "employees"), {
-        name: name,
-        position: position,
-        salary: Number(salary)
-      });
+  querySnapshot.forEach((doc)=>{
 
-      alert("Employee Saved!");
+    const employee = doc.data();
 
-    } catch (error) {
-
-      alert(error.message);
-
-    }
+    employeeList.innerHTML += `
+      <tr>
+        <td>${employee.name}</td>
+        <td>${employee.position}</td>
+        <td>₱${employee.salary}</td>
+      </tr>
+    `;
 
   });
 
 }
+
+if(saveBtn){
+
+  saveBtn.addEventListener("click", async ()=>{
+
+    const name =
+    document.getElementById("name").value;
+
+    const position =
+    document.getElementById("position").value;
+
+    const salary =
+    document.getElementById("salary").value;
+
+    await addDoc(
+      collection(db,"employees"),
+      {
+        name:name,
+        position:position,
+        salary:Number(salary)
+      }
+    );
+
+    alert("Employee Saved!");
+
+    loadEmployees();
+
+  });
+
+}
+
+loadEmployees();
