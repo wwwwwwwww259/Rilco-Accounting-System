d8a61a6fe04d3f9c06dc2e31f4cbcb0e80509769
wwwwwwwww259 <wakshakob@gmail.com>
2026-06-11 18:32:33 +0800

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,37 @@
-alert("AUTH LOADED");
+import { db } from "./firebase.js";
+
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+console.log("AUTH LOADED");
+
+async function loadEmployees() {
+
+    const employeeList =
+    document.getElementById("employeeList");
+
+    employeeList.innerHTML = "";
+
+    const querySnapshot =
+    await getDocs(collection(db,"employees"));
+
+    console.log("TOTAL:", querySnapshot.size);
+
+    querySnapshot.forEach((doc)=>{
+
+        const employee = doc.data();
+
+        employeeList.innerHTML += `
+        <tr>
+            <td>${employee.name}</td>
+            <td>${employee.position}</td>
+            <td>₱${employee.salary}</td>
+        </tr>
+        `;
+    });
+
+}
+
+loadEmployees();
